8b347d14cccb7002210cb21092387792b3d36957
Heltson <you@example.com>
2025-11-03 14:34:23 +0100

    Lesson  6 - Enqueue Admin Scripts

--- wp-app/wp-content/plugins/my-plugin/my-plugin.php
+++ wp-app/wp-content/plugins/my-plugin/my-plugin.php
@@ -23,6 +23,11 @@ class MyPlugin
 		add_action('init', [$this, 'custom_post_type']);
 	}
 
+	public function register()
+	{
+		add_action('admin_enqueue_scripts', [$this, 'enqueue']);
+	}
+
 	public function activate()
 	{
 		$this->custom_post_type();
@@ -38,10 +43,17 @@ class MyPlugin
 	{
 		register_post_type('Book',  ['public' => true, 'label' => 'Books']);
 	}
+
+	public function enqueue()
+	{
+		wp_enqueue_style('mypluginstyle', plugins_url('/assets/mypluginstyle.css', __FILE__));
+		wp_enqueue_script('mypluginstyle', plugins_url('/assets/mypluginscript.js', __FILE__));
+	}
 }
 
 if (class_exists('MyPlugin')) {
 	$myPlugin = new MyPlugin();
+	$myPlugin->register();
 }
 
 register_activation_hook(__FILE__, [$myPlugin, 'activate']);
